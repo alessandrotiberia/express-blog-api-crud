0871e70009230289cc0763f9e85faf16f7f7d973
Alessandro Tiberia <tiberia7030@gmail.com>
2026-05-25 15:48:28 +0200

    collegata porta nel file server correttamente

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.SERVER_PORT || 3000;
+
+app.listen(port, (Error) => {
+    if(Error ) {
+        console.error("errore nel caricamento");
+        return;
+    }
+    console.log(`server aperto porta ${port}`);
+    
+});
