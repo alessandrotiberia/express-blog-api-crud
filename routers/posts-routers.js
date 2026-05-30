2a6812002a0f110a2dc09293fb3f1e68af45e18f
Alessandro Tiberia <tiberia7030@gmail.com>
2026-05-30 10:35:34 +0200

    esercizio del 27 completato

--- routers/posts-routers.js
+++ routers/posts-routers.js
@@ -1,12 +1,13 @@
 import express from 'express';
 
-import { index, show, destroy, store } from '../controllers/posts-controllers.js';
+import { index, show, destroy, store, update } from '../controllers/posts-controllers.js';
+import validatePost from '../middleware/validatePost.js';
 
 const router = express.Router();
 
 router.get('/', index);
 router.get('/:id', show);
 router.delete ('/:id', destroy),
-router.post ('/', store);
-
+router.post ('/',validatePost, store);
+router.put('/:id', validatePost, update);
 export default router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import router from './routers/posts-routers.js';
+import notFound from './middlewares/notFound.js';
+import errorsHandler from './middlewares/errorsHandler.js';
 
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
@@ -9,6 +11,9 @@ const port = process.env.SERVER_PORT || 3000;
 app.use(express.json());
 
 app.use('/posts',router); // ricevi richiesta get vai a postrouter
+
+app.use(notFound);
+app.use(errorsHandler); //deve essere ultimo
 
 app.listen(port, (Error) => {
     if(Error ) {
